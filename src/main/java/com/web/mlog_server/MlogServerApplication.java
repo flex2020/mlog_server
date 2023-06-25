68c848c36e243df03f01f1d8d1efeb6ecff2bfb4
NOH UIBIN <hosan_1@naver.com>
2023-06-25 18:58:28 +0900

    Initial commit

---
+++ src/main/java/com/web/mlog_server/MlogServerApplication.java
@@ -0,0 +1,13 @@
+package com.web.mlog_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlogServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlogServerApplication.class, args);
+	}
+
+}
