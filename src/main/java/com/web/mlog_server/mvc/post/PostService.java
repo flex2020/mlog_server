9fbb35b0e7f4425d42b1af79c5e7421a1a7b42ad
NOH UIBIN <hosan_1@naver.com>
2023-08-05 14:43:51 +0900

    fix: 오타 수정

--- src/main/java/com/web/mlog_server/mvc/post/PostService.java
+++ src/main/java/com/web/mlog_server/mvc/post/PostService.java
@@ -55,7 +55,7 @@ public class PostService {
     @Transactional
     public boolean deletePost(int id) {
         Post post = postRepository.findByIdAndVisibleIsTrue(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 포스트입니다."))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 포스트입니다."));
         try {
             post.setVisible(false);
         } catch (Exception e) {
