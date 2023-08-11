054b3667c6015e4978b6d90db3eb20dc77e01d81
NOH UIBIN <hosan_1@naver.com>
2023-08-11 20:19:53 +0900

    feat: 권한 확인용 API 설정

--- src/main/java/com/web/mlog_server/mvc/admin/AdminController.java
+++ src/main/java/com/web/mlog_server/mvc/admin/AdminController.java
@@ -4,6 +4,7 @@ import com.web.mlog_server.mvc.admin.model.AdminDto;
 import com.web.mlog_server.security.TokenInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +20,10 @@ public class AdminController {
     @PostMapping("/login")
     public TokenInfo login(@RequestBody AdminDto.LoginDto dto) {
         return adminService.login(dto.getId(), dto.getPassword());
+    }
+
+    @PostMapping("/auth")
+    public boolean isAuth() {
+        return true;
     }
 }
