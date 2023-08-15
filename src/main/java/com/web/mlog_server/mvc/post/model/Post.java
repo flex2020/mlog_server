fbc92e4f8558c2944e7ce28dfdba9857bda8d6fe
NOH UIBIN <hosan_1@naver.com>
2023-08-15 17:19:25 +0900

    update: Getter 추가

--- src/main/java/com/web/mlog_server/mvc/post/model/Post.java
+++ src/main/java/com/web/mlog_server/mvc/post/model/Post.java
@@ -4,6 +4,7 @@ import com.web.mlog_server.mvc.admin.AdminDto;
 import com.web.mlog_server.mvc.post.PostDto;
 import jakarta.persistence.*;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -13,6 +14,7 @@ import java.util.List;
 
 @Entity
 @NoArgsConstructor
+@Getter
 @SequenceGenerator(
         name =  "POST_SEQ_GEN", // 시퀀스 제너레이터 이름
         sequenceName = "POST_SEQ", // 시퀀스 이름
