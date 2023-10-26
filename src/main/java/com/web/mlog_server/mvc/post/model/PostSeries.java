bab30a0f0bf149526fbbf7eca56709e25349f807
NOH UIBIN <hosan_1@naver.com>
2023-10-26 20:11:56 +0900

    update: 시퀀스 수정

--- src/main/java/com/web/mlog_server/mvc/post/model/PostSeries.java
+++ src/main/java/com/web/mlog_server/mvc/post/model/PostSeries.java
@@ -15,7 +15,7 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostSeries {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(length = 20)
